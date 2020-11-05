a60f9aae9e62c4648d3611e2e88880521b549e6e
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-11-05 08:51:00 -0500

    ConnectedtoOculus2

---
+++ VRappProject1TheFort/Temp/gradleOut/launcher/build/generated/source/buildConfig/release/com/BrooklynTravelsLLC/VRappProject1TheFort/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.BrooklynTravelsLLC.VRappProject1TheFort;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.BrooklynTravelsLLC.VRappProject1TheFort";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
